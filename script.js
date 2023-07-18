3efcf6c02b8d7bcccee494690557bb65f0db29c7
Bhuminandan <134225833+Bhuminandan@users.noreply.github.com>
2023-07-18 16:16:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function display_ct5() {
   let ampm = x.getHours( ) >= 12 ? ' PM' : ' AM';
   
   let x1 = x.getMonth() + 1 + "/" + x.getDate() + "/" + x.getFullYear(); 
-  x1 = x1 + ", " +  x.getHours( )+ ":" +  x.getMinutes() + ":" +  x.getSeconds() + ":" + ampm;
+  x1 = x1 + ", " +  x.getHours( )+ ":" +  x.getMinutes() + ":" +  x.getSeconds() + " " + ampm;
   document.getElementById('timer').innerHTML = x1;
   display_c5();
    }
